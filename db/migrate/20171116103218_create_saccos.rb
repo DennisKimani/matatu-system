010899d0db6500a3d56d43652ad42361c74610cc
DennisKimani <dennisngugi.dk@gmail.com>
2017-11-23 16:22:45 +0300

    migrated the sacco to save 10 integers.

--- db/migrate/20171116103218_create_saccos.rb
+++ db/migrate/20171116103218_create_saccos.rb
@@ -3,9 +3,9 @@ class CreateSaccos < ActiveRecord::Migration[5.1]
     create_table :saccos do |t|
       t.column :name, :string
       t.column :email, :string
-      t.column :phone, :integer
+      t.column :phone, :integer, null: false, limit: 10, unique: true
       t.column :logo, :string
-      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171116103218) do
   create_table "saccos", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "phone"
+    t.bigint "phone", null: false
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
