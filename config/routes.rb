7feebaae734cfec0afb8d0f026267cc92e238f4c
DennisKimani <dennisngugi.dk@gmail.com>
2017-12-01 09:24:32 +0300

    added resource for gallaries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :galleries
   resources :saccos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'saccos#index'
